6bb5f186ba895c8fbbddc9a1098317269880b512
Burke Holland <burkeholland@Burkes-MacBook-Pro.local>
2017-10-14 11:07:30 -0500

    Add Build

---
+++ env/environment.js
@@ -0,0 +1,10 @@
+const cosmosPort = 10255; // replace with your port
+const dbName = 'react-cosmos-db';
+const key =
+  'swQOhAsVjfHx3Q9VXh29T9U8xQNVGQ78lEQaL6yMNq3rOSA1WhUXHTOcmDf38QMyAe0ESex8rg14NHtQLcUuMA==';
+
+module.exports = {
+  cosmosPort,
+  dbName,
+  key
+};
